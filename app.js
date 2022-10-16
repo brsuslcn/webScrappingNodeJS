19fd73841db59b5b460c50f2aa1e5ffb5f56785a
Barış USLUCAN <47759665+brsuslcn@users.noreply.github.com>
2022-10-17 00:56:54 +0300

    second commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const { response } = require('express');
+const express = require('express');
+
+var articles = [];
+
+
+var app = express();
+
+const url = 'http://www.bologna.yildiz.edu.tr/index.php?r=program/view&id=37&aid=24';
+
+axios(url).then(response => {
+const html = response.data;
+const $ = cheerio.load(html);
+
+$(".semesters > table > tr > td", html).each(function()
+{
+const title = $(this).text();
+articles.push({
+    title
+});
+
+
+});
+
+console.log(articles);
+
+
+});
+
+app.listen(3000);
